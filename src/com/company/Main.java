30ed819da91beb125b4bfd616b14ae98f7db29aa
Rinat Uzanzaev <wilax175@gmail.com>
2022-06-22 12:10:20 +0600

    add counter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 
 public class Main {
 
+    static int playerWins = 0, botWins = 0, draws = 0, totalGames = 0;
+
     public static void main(String[] args) {
         run();
     }
