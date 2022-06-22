f9af9c5e2323a4ef407d7ffd6f3e732e6dca7417
Rinat Uzanzaev <wilax175@gmail.com>
2022-06-22 12:20:29 +0600

    creating an infinite loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,65 +15,80 @@ public class Main {
 
     private static void run() {
         Actions[] actions = Actions.values();
-        System.out.print("| ");
-        for (Actions a : actions) {
-            System.out.print(a + " | ");
-        }
-        System.out.println();
-        System.out.print("Select the sign you want to select by its number (1 - 3): ");
-        int playerChoice = getNumber() - 1;
-
-        while (playerChoice < 0 || playerChoice > 2) {
-            System.out.print("Invalid value. Enter from 1 to 3: ");
-            playerChoice = getNumber() - 1;
-        }
-
-        Actions player = actions[playerChoice];
-        Actions playerBot = actions[playerBot()];
-
-        totalGames++;
-
-        if (player == Actions.ROCK) {
-            if (playerBot == Actions.PAPER) {
-                printActions(player, playerBot);
-                System.out.println("You lose!");
-                botWins++;
-            } else if (playerBot == Actions.SCISSORS) {
-                printActions(player, playerBot);
-                System.out.println("You win!");
-                playerWins++;
-            } else {
-                printActions(player, playerBot);
-                System.out.println("Draw");
-                draws++;
+        while (true) {
+            System.out.print("| ");
+            for (Actions a : actions) {
+                System.out.print(a + " | ");
             }
-        } else if (player == Actions.PAPER) {
-            if (playerBot == Actions.PAPER) {
-                printActions(player, playerBot);
-                System.out.println("Draw");
-                draws++;
-            } else if (playerBot == Actions.SCISSORS) {
-                printActions(player, playerBot);
-                System.out.println("You lose!");
-                botWins++;
-            } else {
-                printActions(player, playerBot);
-                System.out.println("You win!");
-                playerWins++;
+            System.out.println();
+            System.out.print("Select the sign you want to select by its number (1 - 3): ");
+            int playerChoice = getNumber() - 1;
+
+            while (playerChoice < 0 || playerChoice > 2) {
+                System.out.print("Invalid value. Enter from 1 to 3: ");
+                playerChoice = getNumber() - 1;
             }
-        } else {
-            if (playerBot == Actions.PAPER) {
-                printActions(player, playerBot);
-                System.out.println("You win!");
-                playerWins++;
-            } else if (playerBot == Actions.SCISSORS) {
-                printActions(player, playerBot);
-                System.out.println("Draw");
-                draws++;
+
+            Actions player = actions[playerChoice];
+            Actions playerBot = actions[playerBot()];
+
+            totalGames++;
+
+            if (player == Actions.ROCK) {
+                if (playerBot == Actions.PAPER) {
+                    printActions(player, playerBot);
+                    System.out.println("You lose!");
+                    botWins++;
+                } else if (playerBot == Actions.SCISSORS) {
+                    printActions(player, playerBot);
+                    System.out.println("You win!");
+                    playerWins++;
+                } else {
+                    printActions(player, playerBot);
+                    System.out.println("Draw");
+                    draws++;
+                }
+            } else if (player == Actions.PAPER) {
+                if (playerBot == Actions.PAPER) {
+                    printActions(player, playerBot);
+                    System.out.println("Draw");
+                    draws++;
+                } else if (playerBot == Actions.SCISSORS) {
+                    printActions(player, playerBot);
+                    System.out.println("You lose!");
+                    botWins++;
+                } else {
+                    printActions(player, playerBot);
+                    System.out.println("You win!");
+                    playerWins++;
+                }
             } else {
-                printActions(player, playerBot);
-                System.out.println("You lose!");
-                botWins++;
+                if (playerBot == Actions.PAPER) {
+                    printActions(player, playerBot);
+                    System.out.println("You win!");
+                    playerWins++;
+                } else if (playerBot == Actions.SCISSORS) {
+                    printActions(player, playerBot);
+                    System.out.println("Draw");
+                    draws++;
+                } else {
+                    printActions(player, playerBot);
+                    System.out.println("You lose!");
+                    botWins++;
+                }
+            }
+
+            System.out.print("Do you want to complete the game? (y/n): ");
+            String endGame = getString(new Scanner(System.in)).toLowerCase();
+
+            while (!endGame.equals("y") && !endGame.equals("n")) {
+                System.out.print("Please enter 'y' or 'n': ");
+                endGame = getString(new Scanner(System.in)).toLowerCase();
+            }
+            System.out.println();
+
+            if (endGame.equals("y")) {
+                break;
             }
         }
     }
@@ -97,5 +112,20 @@ public class Main {
     private static void printActions(Actions player, Actions bot) {
         System.out.println("Player: " + player);
         System.out.println("Bot: " + bot);
+    }
+
+    private static String getString(Scanner sc) {
+        String str = "";
+        while (sc.hasNextLine()) {
+            try {
+                str += sc.next("[a-zA-Z]+");
+                break;
+            } catch (InputMismatchException e) {
+                System.out.print("Invalid value. Try again: ");
+                sc.nextLine();
+            }
+        }
+
+        return str;
     }
 }
