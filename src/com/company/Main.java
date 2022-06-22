c943cf3ac93002d89c50b5caca6cc08fc4121b49
Rinat Uzanzaev <wilax175@gmail.com>
2022-06-22 12:08:22 +0600

    initialize a list of actions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,18 @@
 package com.company;
 
+
 public class Main {
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    private static void run() {
+        Actions[] actions = Actions.values();
+
+
+    }
+
 
 
 }
